36151cebbbecdea0507e14db2983bf7399e00dd5
jitesh <jiteshjitsun@gmail.com>
2021-09-17 22:56:16 +0530

    oderitem_set.all() error in views.py

--- Home/models.py
+++ Home/models.py
@@ -22,6 +22,14 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def imgURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ""
+        return url
+
 # orders model 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -31,14 +39,20 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
-
+    
     @property
-    def imgURL(self):
-        try:
-            url = self.image.url
-        except:
-            url = ""
-        return url
+    def get_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+    
 
 # orders items
 class OrderItems(models.Model):
@@ -46,6 +60,11 @@ class OrderItems(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 # shipping model
 class ShippingAddress(models.Model):
--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Customer, Product, Order
 
 def home(request):
     products = Product.objects.all()
@@ -7,7 +7,14 @@ def home(request):
     return render(request, 'Home/home.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_total': 0, 'get_cart_items': 0 }
+    context = {'items':items, 'order':order}
     return render(request, 'Home/cart.html', context)
 
 def checkout(request):
