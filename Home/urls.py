d8102bf7ce2cedd57f19283837b9ffa237cfe6c3
jitesh <jiteshjitsun@gmail.com>
2021-10-08 22:51:57 +0530

    workin on cart page

--- Home/urls.py
+++ Home/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path('', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
+    path('update_cart', views.updateItem, name="update_cart")
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Customer, Product, Order
 
 def home(request):
@@ -27,3 +28,6 @@ def checkout(request):
         order = {'get_total': 0, 'get_cart_items': 0 }
     context = {'items':items, 'order':order}
     return render(request, 'Home/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe= False)
